a00ec9aff640b3fd53e4a724a27cfa762a670214
Mike Henke <henke.mike@gmail.com>
2013-05-18 22:14:28 -0500

    changes

--- Vagrantfile
+++ Vagrantfile
@@ -1,6 +1,6 @@
 Vagrant.configure("2") do |config|
   config.vm.provider :virtualbox do |vb|
-    vb.customize ["modifyvm", :id, "--memory", "1024"]
+    vb.customize ["modifyvm", :id, "--memory", "2048"]
     config.vm.network :forwarded_port, guest: 8080, host: 8181, auto_correct: true
   end
 end
--- install_jenkins.sh
+++ install_jenkins.sh
@@ -8,22 +8,18 @@ sudo aptitude install jenkins
 #sudo apt-get upgrade
 
 echo install plugins
-wget -P /var/lib/jenkins/plugins http://updates.jenkins-ci.org/latest/git.hpi
+
 wget -P /var/lib/jenkins/plugins http://updates.jenkins-ci.org/latest/violations.hpi
-wget -P /var/lib/jenkins/plugins http://updates.jenkins-ci.org/latest/terminal.hpi
-wget -P /var/lib/jenkins/plugins http://updates.jenkins-ci.org/latest/jslint.hpi
 wget -P /var/lib/jenkins/plugins http://updates.jenkins-ci.org/latest/warnings.hpi
-wget -P /var/lib/jenkins/plugins http://updates.jenkins-ci.org/latest/tasks.hpi
-wget -P /var/lib/jenkins/plugins http://updates.jenkins-ci.org/latest/checkstyle.hpi
 wget -P /var/lib/jenkins/plugins http://updates.jenkins-ci.org/latest/analysis-core.hpi
 wget -P /var/lib/jenkins/plugins http://updates.jenkins-ci.org/latest/dry.hpi
 wget -P /var/lib/jenkins/plugins http://updates.jenkins-ci.org/latest/ant.hpi
-wget -P /var/lib/jenkins/plugins http://updates.jenkins-ci.org/latest/dashboard-view.hpi
-wget -P /var/lib/jenkins/plugins http://updates.jenkins-ci.org/latest/disk-usage.hpi
-wget -P /var/lib/jenkins/plugins http://updates.jenkins-ci.org/latest/build-failure-analyzer.hpi
-wget -P /var/lib/jenkins/plugins http://updates.jenkins-ci.org/latest/htmlpublisher.hpi
+wget -P /var/lib/jenkins/plugins http://updates.jenkins-ci.org/latest/git.hpi
+wget -P /var/lib/jenkins/plugins http://updates.jenkins-ci.org/latest/tasks.hpi
 wget -P /var/lib/jenkins/plugins http://updates.jenkins-ci.org/latest/heroku-jenkins-plugin.hpi
 wget -P /var/lib/jenkins/plugins http://updates.jenkins-ci.org/latest/github.hpi
 wget -P /var/lib/jenkins/plugins http://updates.jenkins-ci.org/latest/github-api.hpi
+wget -P /var/lib/jenkins/plugins http://updates.jenkins-ci.org/latest/compact-columns.hpi
+wget -P /var/lib/jenkins/plugins http://updates.jenkins-ci.org/latest/tmpcleaner.hpi
 
 service jenkins restart
