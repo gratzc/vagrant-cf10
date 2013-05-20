a5b5d2b0f6ed554e4836d56dd1e63903a2131277
Mike Henke <henke.mike@gmail.com>
2013-05-19 20:48:19 -0500

    fixing install jenkins

--- install_jenkins.sh
+++ install_jenkins.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo setup jenkins
+echo add jenkins sources.list
 wget -q -O - http://pkg.jenkins-ci.org/debian/jenkins-ci.org.key | sudo apt-key add -
 sudo sh -c 'echo deb http://pkg.jenkins-ci.org/debian binary/ > /etc/apt/sources.list.d/jenkins.list'
 
